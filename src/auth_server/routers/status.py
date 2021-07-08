2d789b6b471d843df6717cb3f0390fc3cba6b7e3
Johan Lundberg <lundberg@sunet.se>
2021-07-08 14:50:17 +0200

    added optional health check for mongodb

--- src/auth_server/routers/status.py
+++ src/auth_server/routers/status.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+import asyncio
 import logging
 
 from fastapi import APIRouter
+from pymongo.errors import ConnectionFailure
 
+from auth_server.config import load_config
+from auth_server.db.client import get_mongodb_client
 from auth_server.models.status import Status, StatusResponse
 
 __author__ = 'lundberg'
@@ -13,7 +17,26 @@ logger = logging.getLogger(__name__)
 status_router = APIRouter(prefix='/status')
 
 
+async def _mongodb_health_check():
+    config = load_config()
+    if config.mongo_uri is None:
+        # no mongodb configured
+        return True
+
+    client = await get_mongodb_client()
+    try:
+        await client.admin.command('ismaster')
+        return True
+    except ConnectionFailure as e:
+        logging.error(f'mongodb not healthy: {e}')
+        return False
+
+
 @status_router.get('/healthy', response_model=StatusResponse, response_model_exclude_unset=True)
 async def healthy():
-    # TODO: Implement a real health check
-    return StatusResponse(status=Status.OK)
+    checks = [_mongodb_health_check()]
+    check_results = await asyncio.gather(*checks)
+    if all(check_results):
+        return StatusResponse(status=Status.OK)
+    else:
+        return StatusResponse(status=Status.FAIL)
