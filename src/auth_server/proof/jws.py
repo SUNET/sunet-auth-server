776cdb4380d0f09eb720d70239650aac7962e6d4
Johan Lundberg <lundberg@sunet.se>
2022-08-16 16:31:10 +0200

    load gnap request from unverified jws and defer validation until proof

--- src/auth_server/proof/jws.py
+++ src/auth_server/proof/jws.py
@@ -5,7 +5,7 @@ from typing import Optional, Union
 
 from cryptography.hazmat.primitives.hashes import SHA256, SHA384, SHA512
 from fastapi import HTTPException
-from jwcrypto import jws
+from jwcrypto import jwk, jws
 from jwcrypto.common import base64url_encode
 from pydantic import ValidationError
 
@@ -64,13 +64,13 @@ async def verify_gnap_jws(
         raise HTTPException(status_code=400, detail='http uri does not match')
 
     # The hashed access token used for this request
-    # The the result of Base64url encoding (with no padding) of the SHA-256 digest of the ASCII encoding of the
+    # The result of Base64url encoding (with no padding) of the SHA-256 digest of the ASCII encoding of the
     # associated access token's value.
     if access_token is not None:
         access_token_hash = hash_with(SHA256(), access_token.encode())
-        b64_access_token_hash = urlsafe_b64encode(access_token_hash)
+        b64_access_token_hash = urlsafe_b64encode(access_token_hash).decode('utf-8')
         if b64_access_token_hash != jws_header.ath:
-            logger.error(f'ath mismatch. calculated ath: {b64_access_token_hash!r} != header: {jws_header.ath!r}')
+            logger.error(f'ath mismatch. calculated ath: {b64_access_token_hash} != header: {jws_header.ath}')
             raise HTTPException(status_code=400, detail='ath does not match')
     return True
 
@@ -78,31 +78,43 @@ async def verify_gnap_jws(
 async def check_jws_proof(
     request: ContextRequest,
     gnap_key: Key,
-    jws_header: GNAPJOSEHeader,
     access_token: Optional[str] = None,
 ) -> bool:
-    if request.context.jws_verified:
-        if jws_header.typ is not SupportedJWSType.JWS:
-            raise HTTPException(status_code=400, detail=f'typ should be {SupportedJWSType.JWS}')
-        return await verify_gnap_jws(
-            request=request, gnap_key=gnap_key, jws_header=jws_header, access_token=access_token
-        )
-    return False
+    # Verify jws
+    if request.context.jws_obj is None:
+        raise HTTPException(status_code=400, detail='No JWS found')
+
+    verify_jws(jws_obj=request.context.jws_obj, gnap_key=gnap_key)
+
+    # Parse jws header
+    try:
+        jws_header = GNAPJOSEHeader(**request.context.jws_obj.jose_header)
+    except ValidationError as e:
+        logger.error('Missing JWS header')
+        raise HTTPException(status_code=400, detail=f'Missing JWS header: {e}')
+
+    if jws_header.typ is not SupportedJWSType.JWS:
+        raise HTTPException(status_code=400, detail=f'typ should be {SupportedJWSType.JWS}')
+
+    return await verify_gnap_jws(request=request, gnap_key=gnap_key, jws_header=jws_header, access_token=access_token)
 
 
 async def check_jwsd_proof(
     request: ContextRequest,
     gnap_key: Key,
     gnap_request: Union[GrantRequest, ContinueRequest],
-    detached_jws: str,
     key_reference: Optional[str] = None,
     access_token: Optional[str] = None,
 ) -> bool:
-    logger.debug(f'detached_jws: {detached_jws}')
+
+    if request.context.detached_jws is None:
+        raise HTTPException(status_code=400, detail='No detached JWS found')
+
+    logger.debug(f'detached_jws: {request.context.detached_jws}')
 
     # recreate jws
     try:
-        header, _, signature = detached_jws.split('.')
+        header, _, signature = request.context.detached_jws.split('.')
     except ValueError as e:
         logger.error(f'invalid detached jws: {e}')
         return False
@@ -129,19 +141,7 @@ async def check_jwsd_proof(
         logger.error(f'Failed to deserialize detached jws: {e}')
         return False
 
-    # verify jws
-    client_key = None
-    if gnap_key.jwk is not None:
-        client_key = jws.JWK(**gnap_key.jwk.dict(exclude_unset=True))
-    if client_key is not None:
-        try:
-            _jws.verify(client_key)
-            logger.info('Detached JWS token verified')
-        except jws.InvalidJWSSignature as e:
-            logger.error(f'JWS signature failure: {e}')
-            raise HTTPException(status_code=400, detail='detached JWS signature could not be verified')
-    else:
-        raise HTTPException(status_code=400, detail='no client key found')
+    verify_jws(jws_obj=_jws, gnap_key=gnap_key)
 
     try:
         jws_header = GNAPJOSEHeader(**_jws.jose_header)
@@ -153,3 +153,18 @@ async def check_jwsd_proof(
         raise HTTPException(status_code=400, detail=f'typ should be {SupportedJWSType.JWSD}')
 
     return await verify_gnap_jws(request=request, gnap_key=gnap_key, jws_header=jws_header, access_token=access_token)
+
+
+def verify_jws(jws_obj: jws.JWS, client_key: Optional[jwk.JWK] = None, gnap_key: Optional[Key] = None) -> bool:
+    if gnap_key is not None and gnap_key.jwk is not None:
+        client_key = jws.JWK(**gnap_key.jwk.dict(exclude_unset=True))
+    if client_key is not None:
+        try:
+            jws_obj.verify(client_key)
+            logger.info('JWS token verified')
+            return True
+        except jws.InvalidJWSSignature as e:
+            logger.error(f'JWS signature failure: {e}')
+            raise HTTPException(status_code=400, detail='JWS signature could not be verified')
+    else:
+        raise HTTPException(status_code=400, detail='no client key found')
