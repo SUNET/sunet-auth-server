d4520b7971bcade9d356476fcfc08e1ec703e831
Johan Lundberg <lundberg@sunet.se>
2023-10-30 14:27:49 +0100

    even better logging

--- src/auth_server/log.py
+++ src/auth_server/log.py
@@ -1,34 +1,20 @@
 # -*- coding: utf-8 -*-
-import logging
+import logging.config
 import sys
 from typing import Optional
 
 from loguru import logger
 
+from auth_server.log_handler import InterceptHandler
+
 __author__ = "lundberg"
+
 
 LOGURU_FORMAT = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> |\
  <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
 
 
-class InterceptHandler(logging.Handler):
-    @logger.catch(default=True, onerror=lambda _: sys.exit(1))
-    def emit(self, record):
-        # Get corresponding Loguru level if it exists
-        try:
-            level = logger.level(record.levelname).name
-        except ValueError:
-            level = record.levelno
-
-        # Find caller from where originated the logged message
-        frame, depth = logging.currentframe(), 2
-        while frame.f_code.co_filename == logging.__file__:
-            frame = frame.f_back
-            depth += 1
-
-        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
-
-
+# set up default python logger to be intercepted by loguru
 logging.getLogger().handlers = [InterceptHandler()]
 logging.getLogger().level = 0  # DEBUG
 
